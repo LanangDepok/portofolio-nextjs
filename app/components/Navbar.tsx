4dddf8ac8929e23543a9d68f8d478d360bae3689
LanangDepok <bagasrizkiyanto@gmail.com>
2025-08-24 04:11:40 +0700

    memberitahu halaman yang aktif di navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { MdGTranslate } from "react-icons/md";
 import { ToggleContext } from "../context/ToggleContext";
 import { LocaleContext } from "../context/LocaleContext";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const { toggleValue, setToggleValue } = useContext(ToggleContext);
   const [localeValue, toggleLocale] = useContext(LocaleContext);
   const { closeNavbar } = useContext(ToggleContext);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    console.log(pathname);
+  });
 
   return (
     <div className="top-0 right-0 left-0 z-10 fixed flex justify-between items-center bg-linear-to-t from-cyan-800 to-black px-5 h-20 font-bold">
@@ -38,33 +44,41 @@ export default function Navbar() {
       </div>
       <div
         className={`${
-          toggleValue ? "translate-x-40 md:translate-x-48" : ""
+          toggleValue && "translate-x-40 md:translate-x-48"
         } transition duration-600 flex flex-col lg:flex-row lg:justify-end gap-7 lg:gap-5 fixed lg:static w-40 md:w-48 lg:w-1/2 top-20 bottom-0 -left-40 md:-left-48 lg:translate-none bg-linear-to-bl from-cyan-800 to-black lg:bg-none`}
       >
         <Link
           href="/"
-          className="hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2"
+          className={`${
+            pathname === "/" && "underline underline-offset-8"
+          }  hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2`}
           onClick={() => closeNavbar()}
         >
           <div>{localeValue === "en" ? "Introduction" : "Perkenalan"}</div>
         </Link>
         <Link
           href="/skills"
-          className="hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2"
+          className={`${
+            pathname === "/skills" && "underline underline-offset-8"
+          }  hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2`}
           onClick={() => closeNavbar()}
         >
           <div>{localeValue === "en" ? "Skills" : "Keahlian"}</div>
         </Link>
         <Link
           href="/experiences"
-          className="hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2"
+          className={`${
+            pathname === "/experiences" && "underline underline-offset-8"
+          }  hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2`}
           onClick={() => closeNavbar()}
         >
           <div>{localeValue === "en" ? "Experiences" : "Pengalaman"}</div>
         </Link>
         <Link
           href="/certificates"
-          className="hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2"
+          className={`${
+            pathname === "/certificates" && "underline underline-offset-8"
+          }  hover:bg-linear-to-l hover:from-white hover:to-gray-400 px-5 lg:px-0 py-2`}
           onClick={() => closeNavbar()}
         >
           <div>{localeValue === "en" ? "Certificates" : "Sertifikat"}</div>
